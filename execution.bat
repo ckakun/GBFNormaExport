ecd1c20364d9640911f05992a883eefd4ef7d2a6
cka <cka.kun4@gmail.com>
2022-06-28 16:28:51 +0900

    bat files modify

--- execution.bat
+++ execution.bat
@@ -1,7 +1,4 @@
 rem 実行ディレクトリに遷移
 cd %~dp0
-echo %~dp0
 rem ツールを実行
 python NormaExporter.py
-
-pause
--- install.bat
+++ install.bat
@@ -2,5 +2,3 @@ rem 実行ディレクトリに遷移
 cd %~dp0
 rem ツールを実行
 pip install -r resource\requirements.txt
-
-pause
